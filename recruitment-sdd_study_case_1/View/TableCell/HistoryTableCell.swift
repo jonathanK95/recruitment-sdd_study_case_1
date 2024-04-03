9163db7beb79c7a56bca2a0fd0bd961d278d26f4
Jonathan Kristian <jonathankristian1995@gmail.com>
2024-04-03 22:03:36 +0700

    kurang unit test

---
+++ recruitment-sdd_study_case_1/View/TableCell/HistoryTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryTableCell.swift
+//  recruitment-sdd_study_case_1
+//
+//  Created by Jonathan Kristian on 03/04/24.
+//
+
+import UIKit
+
+class HistoryTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
